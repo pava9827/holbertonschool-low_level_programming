6c079bb90cf3cab847fb6acd08fe807794b20e24
pava9827 <gus.rojas2019@protonmail.com>
2021-02-03 18:53:20 -0500

    correcion

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of a char: %lu bytes(s)\n" , sizeof(char));
+printf("size of an int: %lu bytes(s)\n" , sizeof(int);
+printf("size of a long int: %lu bytes(s)\n", sizeof(long int);
+printf("size of a long long: %lu bytes(S)\n" , sizeof(long long int));
+printf("size of a float: %lu bytes(s)\n" , sizeof(float));
+return (0);
+}
+
