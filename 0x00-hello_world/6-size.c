271b43c01fb49f4087a52a15d5e570f362127d5e
pava9827 <gus.rojas2019@protonmail.com>
2021-02-03 22:07:46 -0500

    SIZE2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-printf("size of a char: %lu bytes(s)\n" , sizeof(char));
-printf("size of an int: %lu bytes(s)\n" , sizeof(int);
-printf("size of a long int: %lu bytes(s)\n", sizeof(long int);
-printf("size of a long long: %lu bytes(S)\n" , sizeof(long long int));
-printf("size of a float: %lu bytes(s)\n" , sizeof(float));
-return (0);
+	printf("size of a char: %lu bytes(s)\n" , Sizeof(char));
+	printf("size of an int: %lu bytes(s)\n" , Sizeof(int);
+	printf("size of a long int: %lu bytes(s)\n", Sizeof(long int);
+	printf("size of a long long int: %lu bytes(S)\n" ,Sizeof(long long int));
+	printf("size of a float: %lu bytes(s)\n" , Sizeof(float));
+	return (0);
 }
 
