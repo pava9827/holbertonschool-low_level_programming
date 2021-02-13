15795629ec2f3a47e8a959b9bef69967a5b30863
pava9827 <gus.rojas2019@protonmail.com>
2021-02-12 21:45:36 -0500

    holberton

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,18 @@
 /*a function that checks for uppercase character*/
 int _isupper(int c);
 
-/* a function that checks for a digit (0 through 9 */
+/* c a function that checks for a digit (0 through 9 */
 int _isdigit(int c);
+
+/* a+b  a function that multiplies two integers*/
+int mul(int a, int b);
+
+/**
+ *function that prints the numbers, from 0 to 9, followed by a new 
+ *line.
+ */
+void print_numbers(void);
+/*function that draws a diagonal line on the terminal*/
+void print_diagonal(int n);
+/*writes the character c to stdout*/
+int _putchar(char c);
